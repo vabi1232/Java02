df422a28c94ae8e56a0dce1464005169ba52fab5
Vũ Việt Anh <73330605+vabi1232@users.noreply.github.com>
2021-05-21 15:06:23 +0700

    Update students.sql

--- BaiThi2/students.sql
+++ BaiThi2/students.sql
@@ -4,5 +4,5 @@ StudentName varchar(50),
 Adrees varchar(50),
 Phone int
 );
-insert into students_school values('T2010A','Ngo Chi Thanh Dat','Ha Noi',0394480757),
-('T2011A','Ngo Thanh Dat','Bac Ninh',0386684837);
+insert into students_school values('T2010A','Vu Viet Anh','Ha Noi',0394480757),
+('T2011A','Vu Viet Anh','Ha Noi',0386684837);
